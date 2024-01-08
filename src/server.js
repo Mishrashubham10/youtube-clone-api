c6492422d88e6c88c62f8d7fd63cd0f977657745
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-08 11:52:34 +0530

    Creates middleware for error management and express file

--- src/server.js
+++ src/server.js
@@ -1,35 +1,15 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/db.js";
-import express from "express";
-const app = express();
+import { app } from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
 // DB CONNECTION
-connectDB();
-
-app.listen(PORT, () => {
-    console.log(`Server is runing on ${PORT}`);
-});
-
-// import express from "express";
-
-// const app = express();
-
-// // DB CONNECTION
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-//     app.on("error", (err) => {
-//       console.log("ERRR: ", err);
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (err) {
-//     throw err;
-//   }
-// };
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is up runing at port ${PORT}`));
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed! ", err);
+  });
