bcf29f638df0e4f26a2b882a99aa52ea86d500ff
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-24 16:31:15 +0530

    Creates video controller

---
+++ src/contollers/video.controller.js
@@ -0,0 +1,27 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+// UPLOAD VIDEO
+const getAllVideos = asyncHandler(async (req, res) => {});
+
+// UPDATE VIDEO
+const publishAVideo = asyncHandler(async (req, res) => {});
+
+// DELETE VIDEO
+const getVideoById = asyncHandler(async (req, res) => {});
+
+// UPLOAD VIDEO
+const updateVideo = asyncHandler(async (req, res) => {});
+
+// DELETE VIDEO
+const deleteVideo = asyncHandler(async (req, res) => {});
+
+//
+const togglePublishStatus = asyncHandler(async (req, res) => {});
+export {
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
