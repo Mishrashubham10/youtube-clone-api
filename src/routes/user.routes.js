19312cca3d4b29d5bd19c8d72ff12652f3a128f4
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-24 15:44:41 +0530

    Creates enpoints in route.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { login, logoutUser, refreshAccessToken, registerUser } from "../contollers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  login,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../contollers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -21,12 +33,28 @@ router.route("/register").post(
 );
 
 // LOGIN ROUTE
-router.route("/login").post(login)
-
+router.route("/login").post(login);
 // LOGOUT ROUTE
-router.route("/logout").post(verifyJWT ,logoutUser);
-
+router.route("/logout").post(verifyJWT, logoutUser);
 // REFRESH
 router.route("/refresh-token").post(refreshAccessToken);
+// FORGET PASSWORD
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+// CURRENT USER
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+// UPDATE DETAILS
+router.route("/update-account").patch(verifyJWT, updateAccDetails);
+// AVATAR UPDATE
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+// UPDATE COVERIMAGE
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+// GET PROFILE
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+// WATCH HISTORY
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
