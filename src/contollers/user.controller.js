82b17197036a421eac537940180ad9caba167e5f
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-24 15:33:47 +0530

    Used mongoose aggregate

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { generateAccessAndRefreshTokens } from "../middlewares/token.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // REGISTER USER
 const registerUser = asyncHandler(async (req, res) => {
@@ -390,6 +391,51 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+// GET USER WATCH HISTORY
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+});
+
 export {
   registerUser,
   login,
@@ -401,4 +447,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
