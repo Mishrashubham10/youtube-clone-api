6a79b28998977033d6dd0b7bd3d12ca1cd94d976
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-24 15:36:08 +0530

    Send data to the client

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -434,6 +434,16 @@ const getWatchHistory = asyncHandler(async (req, res) => {
       },
     },
   ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    );
 });
 
 export {
@@ -447,5 +457,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
-  getWatchHistory
+  getWatchHistory,
 };
