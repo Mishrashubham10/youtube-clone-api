dbcceccba2b8a7d8e976dd3c784c02fa54fd212d
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-26 09:18:51 +0530

    Creates controllers

--- src/contollers/comment.controller.js
+++ src/contollers/comment.controller.js
@@ -1,19 +1,65 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/Video.js";
+import { Comment } from "../models/Comment.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/User.js";
 
 // Get Comments
 const getVideoComments = asyncHandler(async (req, res) => {
-    // GET ALL COMMENTS FOR A VIDEO
-    const { videoId } = req.params;
-    const { page = 1, limit = 10 } = req.query;
+  // GET ALL COMMENTS FOR A VIDEO
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  const video = await Video.findById(videoId);
+  const comments = await Comment.find();
+
+  if (!comments) {
+    throw new ApiError(404, "No comments found");
+  }
+
+  res.status(200).json(new ApiResponse(200, comments));
 });
 
 // Add Comment
-const addComment = asyncHandler(async (req, res) => {});
+const addComment = asyncHandler(async (req, res) => {
+    const { content } = req.params
+
+    if (!content) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        throw new ApiError(400, "No such user");
+    }
+
+    const comment = await Comment.create({
+        content,
+    });
+
+    res.status(200).json(
+        new ApiResponse(200, comment, "Comment created successfully")
+    )
+});
 
 // Update Comment
 const updateComment = asyncHandler(async (req, res) => {});
 
 // Delete Comment
-const deleteComment = asyncHandler(async (req, res) => {});
+const deleteComment = asyncHandler(async (req, res) => {
+    const { id } = req.params
+
+    if (!id) {
+        throw new ApiError(400, "Id required")
+    }
+
+    const comment = await Comment.findByIdAndDelete(id, { new: true });
+
+    res.status(200).json(
+        new ApiResponse(200, comment, "Comment deleted successfully")
+    )
+});
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/contollers/video.controller.js
+++ src/contollers/video.controller.js
@@ -1,32 +1,99 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/Video.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 // UPLOAD VIDEO
 const getAllVideos = asyncHandler(async (req, res) => {
-  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
+  // Get all videos from db
+  const videos = await Video.find();
+
+  if (!videos) {
+    throw new ApiError(404, "No videos found!");
+  }
+
+  res.status(200).json(new ApiResponse(200, videos));
 });
 
 // UPDATE VIDEO
 const publishAVideo = asyncHandler(async (req, res) => {
-  const { title, description } = req.body
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    throw new ApiError(400, "All fields are required");
+  }
+
   // get video, upload to cloudinary, create video
+  const videoPath = req.files?.video[0]?.path;
+
+  if (!videoPath) {
+    throw new ApiError(400, "Video file required");
+  }
+
+  const video = await uploadOnCloudinary(videoPath);
+
+  if (!video) {
+    throw new ApiError(400, "Video file required");
+  }
+
+  // Create and save into db
+  const createVideo = await Video.create({
+    title,
+    description,
+    video: video?.url,
+  });
+
+  res.status(200).json(200, createVideo, "Video created successfully");
 });
 
 // DELETE VIDEO
 const getVideoById = asyncHandler(async (req, res) => {
-  const { videoId } = req.params
+  const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Id is required");
+  }
+
+  // get video by id
+  const video = await Video.findById({ videoId });
+
+  if (!video) {
+    throw new ApiError(400, "Video not found");
+  }
+
+  res.status(200).json(new ApiResponse(200, video));
 });
 
 // UPLOAD VIDEO
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Id is required");
+  }
+
   // update video details like title, description, thumbnai
+  const video = await Video.findByIdAndUpdate({ videoId }, { new: true });
 });
 
 // DELETE VIDEO
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
 
+  if (!videoId) {
+    throw new ApiError(400, "Id is required");
+  }
+
   // delete video by id
+  const video = await Video.findByIdAndDelete({ videoId }, { new: true });
+
+  if (!video) {
+    throw new ApiError(404, "Video is required");
+  }
+
+  res.status(200).json(new ApiResponse(200, "Video deleted successfully"));
 });
 
 //
